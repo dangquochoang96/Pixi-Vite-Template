facf8ead9c7642342df1c2ae9cdc866abe527252
Dang Quoc Hoang <quochoangdang96@gmail.com>
2023-07-22 10:23:08 +0700

    change sound manager

--- src/manager/Sound.js
+++ src/manager/Sound.js
@@ -1,143 +1,95 @@
-import { sound } from '@pixi/sound';
-import gsap from 'gsap';
+import { sound } from "@pixi/sound"
+import gsap from "gsap"
 
 /**
- * A class to handle background music within the game.
- * It automatically loops audio and fades between tracks.
+ * Handles music background, playing only one audio file in loop at time,
+ * and fade/stop the music if a new one is requested. Also provide volume
+ * control for music background only, leaving other sounds volumes unchanged.
  */
 class BGM {
-    /** A global volume that affects all bgm sounds. */
-    _globalVolume = 0.15;
-    /** An instance volume that affects the current sound. */
-    _instanceVolume = 0.15;
+  /** Current volume set */
+  volume = 1
 
-    /**
-     * Play background music.
-     * @param alias - Name of the audio file.
-     * @param options - Options to be passed to the sound instance.
-     */
-    async play(alias, options) {
-        // Do nothing if the requested music is already being played
-        if (this.currentAlias === alias) return;
+  /** Play a background music, fading out and stopping the previous, if there is one */
+  async play(alias, options) {
+    // Do nothing if the requested music is already being played
+    if (this.currentAlias === alias) return
 
-        // Fade out then stop current music
-        if (this.current) {
-            const current = this.current;
-
-            gsap.killTweensOf(current);
-            gsap.to(current, { volume: 0, duration: 1, ease: 'linear' }).then(
-                () => {
-                    current.stop();
-                },
-            );
-        }
-
-        // Find out the new instance to be played
-        this.current = sound.find(alias);
-
-        // Play and fade in the new music
-        this.currentAlias = alias;
-        this.current.play({ loop: true, ...options });
-        this.current.volume = 0;
-        gsap.killTweensOf(this.current);
-
-        // Store the instance volume just in case global volume is changed
-        this._instanceVolume = options?.volume ?? 1;
-
-        gsap.to(this.current, {
-            volume: this._globalVolume * this._instanceVolume,
-            duration: 1,
-            ease: 'linear',
-        });
+    // Fade out then stop current music
+    if (this.current) {
+      const current = this.current
+      gsap.killTweensOf(current)
+      gsap.to(current, { volume: 0, duration: 1, ease: "linear" }).then(() => {
+        current.stop()
+      })
     }
 
-    /**
-     * Set the global volume.
-     * @param v - Target volume.
-     */
-    setVolume(v) {
-        this._globalVolume = v;
-        if (this.current)
-            this.current.volume = this._globalVolume * this._instanceVolume;
-    }
+    // Find out the new instance to be played
+    this.current = sound.find(alias)
 
-    /**
-     * Get the global volume.
-     */
-    getVolume() {
-        return this._globalVolume;
-    }
+    // Play and fade in the new music
+    this.currentAlias = alias
+    this.current.play({ loop: true, ...options })
+    this.current.volume = 0
+    gsap.killTweensOf(this.current)
+    gsap.to(this.current, { volume: this.volume, duration: 1, ease: "linear" })
+  }
+
+  /** Get background music volume */
+  getVolume() {
+    return this.volume
+  }
+
+  /** Set background music volume */
+  setVolume(v) {
+    this.volume = v
+    if (this.current) this.current.volume = this.volume
+  }
 }
 
 /**
- * A class to handle sound effects within the game.
+ * Handles short sound special effects, mainly for having its own volume settings.
+ * The volume control is only a workaround to make it work only with this type of sound,
+ * with a limitation of not controlling volume of currently playing instances - only the new ones will
+ * have their volume changed. But because most of sound effects are short sounds, this is generally fine.
  */
 class SFX {
-    /** A global volume that affects all sfx sounds. */
-    _volume = 0.5;
+  /** Volume scale for new instances */
+  volume = 1
 
-    /**
-     * Play sound effects.
-     * @param alias - Name of the audio file.
-     * @param options - Options to be passed to the sound instance.
-     */
-    play(alias, options) {
-        const volume = this._volume * (options?.volume ?? 1);
+  /** Play an one-shot sound effect */
+  play(alias, options) {
+    const volume = this.volume * (options?.volume ?? 1)
+    sound.play(alias, { ...options, volume })
+  }
 
-        sound.play(alias, { ...options, volume });
-    }
+  /** Set sound effects volume */
+  getVolume() {
+    return this.volume
+  }
 
-    /**
-     * Set the global volume.
-     * @param v - Target volume.
-     */
-    setVolume(v) {
-        this._volume = v;
-    }
-
-    /**
-     * Get the global volume.
-     */
-    getVolume() {
-        return this._volume;
-    }
+  /** Set sound effects volume. Does not affect instances that are currently playing */
+  setVolume(v) {
+    this.volume = v
+  }
 }
 
-/**
- * A object to hold methods that handle certain features on the global sound instance
- */
-export const audio = {
-    /**
-     * Mute the global sound instance.
-     * @param value - The audio mute state.
-     */
-    muted(value) {
-        if (value) sound.muteAll();
-        else sound.unmuteAll();
-    },
-    /** Get the volume of the global sound instance. */
-    getMasterVolume() {
-        return sound.volumeAll;
-    },
-    /** Set the volume of the global sound instance.
-     * @param v - The target global volume.
-     */
-    setMasterVolume(v) {
-        sound.volumeAll = v;
-        if (!v) {
-            sound.muteAll();
-        } else {
-            sound.unmuteAll();
-        }
-    },
-};
+/** Get overall sound volume */
+export function getMasterVolume() {
+  return sound.volumeAll
+}
 
-/**
- * A class to handle background music within the game.
- * It automatically loops audio and fades between tracks.
- */
-export const bgm = new BGM();
-/**
- * A class to handle sound effects within the game.
- */
-export const sfx = new SFX();
+/** Set the overall sound volume, affecting all music and sound effects */
+export function setMasterVolume(v) {
+  sound.volumeAll = v
+  if (!v) {
+    sound.muteAll()
+  } else {
+    sound.unmuteAll()
+  }
+}
+/** Shared background music controller */
+export const bgm = new BGM()
+
+/** Shared sound effects controller */
+export const sfx = new SFX()
